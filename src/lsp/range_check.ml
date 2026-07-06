7b11720690c92c3d021fb2d03f0e9007fa2b9a7a
Brandon Stride <brandonzstride@hotmail.com>
2026-07-06 18:25:59 -0400

    more clear check pos for lsp

--- src/lsp/range_check.ml
+++ src/lsp/range_check.ml
@@ -1,17 +1,23 @@
 open Lang.Ast
 
-let compute_check_pos
-  (stmts_with_pos : program_with_pos)
-  (changes : Protocol.range list)
-  : pos_span option =
-  match stmts_with_pos, changes with
-  | [], _ | _, [] -> None
-  | _, first_change :: _ ->
+let compute_check_pos (stmts_with_pos : program_with_pos)
+    (changes : Protocol.range list) : pos_span option =
+  match changes with
+  | [] -> None
+  | first_change :: _ ->
     let target = first_change.start_pos in
-    List.find_mapi (fun i (_stmt, span) ->
+    let contains_target span =
       let stmt_end = Positions.of_lexing span.ends in
-      if Positions.geq stmt_end target then
-        Some (snd (List.nth stmts_with_pos (max 0 (i - 1))))
-      else None
-    ) stmts_with_pos
+      Positions.geq stmt_end target
+    in
+    let rec find = function
+      | [] -> None
+      | (_, span) :: [] -> Some span
+      | (_, prev_span) :: ((_, next_span) :: _ as tl) ->
+        if contains_target next_span then
+          Some prev_span
+        else
+          find tl
+    in
+    find stmts_with_pos
   (* TODO: Skip spawning the typechecker for non-semantic edits (e.g., inserting blank lines). *)
