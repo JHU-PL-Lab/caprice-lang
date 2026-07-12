c27a445a16c02f4cab0ed6b5f2cd7b6c2c50a7cc
Brandon Stride <brandonzstride@hotmail.com>
2026-07-12 13:48:08 -0400

    even fewer comparisons in zipper map

--- src/utils/zipper_map.ml
+++ src/utils/zipper_map.ml
@@ -4,13 +4,15 @@ module Make (Key : Map.OrderedType) = struct
 
   (*
     Persistent map that keeps its pointer at the last addition or lookup
-    because in its use case, it is most common to read and write very near
-    the last access.
+    because in its use case, it is most common to read and write very near the
+    last access. This map is fastest if each read is followed by a read of the
+    next greatest key after the previous read, and each key added is the
+    greatest key.
 
-    The left are keys strictly smaller than the cursor, in descending order.
-    The right are keys greater than or equal to the cursor, in ascending order.
+    The left are keys smaller than or equal to the cursor, in descending order.
+    The right are keys strictly greater than the cursor, in ascending order.
 
-    The left keys are therefore all smaller than the right keys.
+    The left keys are therefore all strictly smaller than the right keys.
   *)
   type 'a t =
     { mutable left : 'a binding list
@@ -19,8 +21,8 @@ module Make (Key : Map.OrderedType) = struct
   (* Must be created fresh to avoid weak type variables *)
   let empty () = { left = [] ; right = [] }
 
-  (** Shift from the right list onto the left list while [key] is at least as
-    large as the head of the right list.
+  (** Shift from the right list onto the left list while [key] greater than or
+    equal to the head of the right list.
     If [key] is strictly less than the right list, or the right list is empty,
     then continues with [with_] instead of shifting. *)
   let rec try_shift_onto_left_until ~with_ ~key left right =
@@ -29,35 +31,33 @@ module Make (Key : Map.OrderedType) = struct
       let c = Key.compare k_right key in
       if c = 0 then
         (* aligned exactly on the desired key *)
-        left, Some v, right
+        binding :: left, Some v, tl_right
       else if c < 0 then
-        (* right head is too small; it needs to be on the left. Shift left
-          and stop once the key is hit. *)
+        (* right head is smaller than the cursor; it needs to be on the left.
+          Shift left until the key is found. *)
         shift_onto_left_until ~key (binding :: left) tl_right
       else
       with_ ~key left right
     | [] ->
       with_ ~key left right
 
-  (** Shift from the right list onto the left list while [key] is still smaller
-    than the right list.
-    Precondition: [key] is at least as large as the head of the right list. *)
+  (** Shift from the right list onto the left list while [key] is still greater
+    than or equal to the head of the right list. *)
   and shift_onto_left_until ~key left right =
     try_shift_onto_left_until ~key left right
       ~with_:(fun ~key:_ l r -> l, None, r)
 
-  (** Shift from the left list onto the right list while [key] is smaller than
-    the head of the left list.
-    Precondition: [key] is at least as small as the head of the left list. *)
+  (** Shift from the left list onto the right list while [key] is strictly
+    smaller than the head of the left list. *)
   let rec shift_onto_right_until ~key left right =
     match left with
     | (k_left, v) as binding :: tl_left ->
       let c = Key.compare k_left key in
       if c = 0 then
-        (* align the key to be on the right *)
-        tl_left, Some v, binding :: right
+        (* Found the key; keep it on the left *)
+        left, Some v, right
       else if c > 0 then
-        (* left head is small; shift from right until we hit the key *)
+        (* left head is greater than the cursor; continue shifting *)
         shift_onto_right_until ~key tl_left (binding :: right)
       else
         left, None, right
@@ -66,17 +66,28 @@ module Make (Key : Map.OrderedType) = struct
 
   (**
     Trade between the left and right lists so that either:
-    - if [key] exists as a key, then it is at the head of right
+    - if [key] exists as a key, then it is at the head of left
     - otherwise all keys in the left are smaller than [key] and all in the right
       are greater than [key]
 
-    This is done by trying to shift from right onto left. But if the right is
-    already above the key, then the key may be contained in the left, so we
-    shift from left onto right instead.
+    Returns the optional value associated with the key. If this value is
+    [Some v], then the left is not empty ([(key, v)] is the head of the left
+    returned list).
+
+    This is done by trying to shift from right onto left first. If the right
+    is already greater than the key, then the key could possibly be in the left,
+    so if shifting onto left fails, then we shift onto the right.
   *)
   let align ~key left right =
     try_shift_onto_left_until ~key left right ~with_:shift_onto_right_until
 
+  (*
+    Notice that after calling find_opt, the cursor is aligned so that the next
+    greatest key is exactly at the head of the right list, which is where
+    alignment begins, so successive calls with successively greater keys (that
+    all exist in the map) are extremely cheap. That is, the map is pre-shifted
+    for the next lookup.
+  *)
   let find_opt key x =
     let left, v_opt, right = align ~key x.left x.right in
     x.left <- left;
@@ -90,7 +101,7 @@ module Make (Key : Map.OrderedType) = struct
   let add key v x =
     let left, v_opt, right = align ~key x.left x.right in
     match v_opt with
-    | Some _ -> { left ; right = (key, v) :: List.tl right }
+    | Some _ -> { left = List.tl left ; right = (key, v) :: right }
     | None -> { left ; right = (key, v) :: right }
 
   let to_list x =
