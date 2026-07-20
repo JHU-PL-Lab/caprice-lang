9f6c65e44efa38aa4d13926fee94a7d28e7c98cb
Brandon Stride <brandonzstride@hotmail.com>
2026-07-20 12:12:54 -0700

    specialise fold_left_funtil

--- src/utils/list_utils.ml
+++ src/utils/list_utils.ml
@@ -1,10 +1,8 @@
 
-let fold_left_until f finish init ls =
-  let rec go acc = function
-    | [] -> finish acc
-    | hd :: tl ->
-      match f acc hd with
-      | `Stop x -> x
-      | `Continue a -> go a tl
-  in
-  go init ls
+let[@specialise] rec fold_left_until f finish acc ls =
+  match ls with
+  | [] -> finish acc
+  | hd :: tl ->
+    match f acc hd with
+    | `Stop x -> x
+    | `Continue a -> fold_left_until f finish a tl
